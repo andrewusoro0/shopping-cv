fa7b7155ccd3f067548e61757e09a0cf5a5db379
andrewusoro0 <67741597+andrewusoro0@users.noreply.github.com>
2020-11-11 16:11:51 +0100

    Add files via upload
    initial website

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+// ======================================//
+function toggleMenuIcon() {
+    document.querySelector('aside').classList.toggle('toggle')
+} 
+
+function toggleBack() {
+    document.querySelector('div').classList.add('toggle')
+}
